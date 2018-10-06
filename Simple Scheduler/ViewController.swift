05c7c142929a5e003488c96568c3db7080ff4054
Alan Li <alan.yilun.li@gmail.com>
2018-10-06 11:44:58 -0700

    Initial Commit

---
+++ Simple Scheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Scheduler
+//
+//  Created by Alan Li on 2018-10-06.
+//  Copyright © 2018 Alan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
